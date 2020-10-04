d446eaad3d59b8ef90ef317071bc8d1288779e04
hamidkj <61036430+hamidkj@users.noreply.github.com>
2020-10-04 12:53:53 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDZ/WYRAkmKqJMDtoO5f0Nro2AW/ny5EH4ME9hPV8MwGLD3sAjzYrc2ABSyn66ouDVX5mhLGjTbQGtLjAgrfgMHziBN410ThShJZXOtNVBMGG5zkvWle1mhDmeUPCt4HmFutdrSAlIcu0+7oy8jAqcIHX6WcXmHJb1SAWW4vMDA5c72nFFQjIrm/xgkOORFc/X/pZTvWGpbXlEpx9NRcbL+5uBPUeYyo9D5DNRQkz/mdo9uQOwjR1gqTaFAsjOnYsvnunG/fU+f+WqJCrLzXT0KaS9WOenqL9xz8bm1K/TPQ7ac5klyiqGwzJwK07z67Sp8nxUiw0YYJ9t19TtN1lEp',
 	}  
 }
