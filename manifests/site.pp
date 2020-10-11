12cbe29ed11967d7206c355ab1c49047dc110ae6
hamidkj <61036430+hamidkj@users.noreply.github.com>
2020-10-11 13:50:10 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,12 +6,3 @@ node default {
   }
 }
 
-node 'minetest.puppet.vm': {
-  include role::minecraft_server
-}
-node /^web/ { 
-  include role::app_server
-}
-node /^db/ {
-  include role::db_server
-}
